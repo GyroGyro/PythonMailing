4fe6fc5251a14f6a6b3c71074dbef4a7166b0d04
GyroGyro <42949338+GyroGyro@users.noreply.github.com>
2021-01-22 16:10:05 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+import smtplib
+import dotenv
+import os
+
+from email import encoders
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.audio import MIMEAudio
+from email.mime.image import MIMEImage
+
+dotenv.load_dotenv('.env')
+
+server = smtplib.SMTP('smtp.gmail.com', 25)
+
+server.starttls()
+
+print(os.getenv('EMAIL'), os.getenv('PASSWORD'))
+
+server.login(os.getenv('EMAIL'), os.getenv('PASSWORD'))
+
+mail = MIMEMultipart()
+
+mail['From'] = "Python Hub"
+mail['To'] = os.getenv('TO')
+mail['Subject'] = 'A gift from Python Hub'
+
+mail.attach(
+    MIMEText("This is an image and audio attachment from Python Hub", 'plain'))
+
+image = open('image.jpg', 'rb')
+audio = open('sample.mp3', 'rb')
+
+imageAttachment = MIMEImage(image.read())
+imageAttachment.add_header('Content-Disposition',
+                           'attachment; filename=image.jpg')
+mail.attach(imageAttachment)
+image.close()
+
+audioAttachment = MIMEAudio(audio.read(), 'mp3')
+audioAttachment.add_header('Content-Disposition',
+                           'attachment; filename=sample.mp3')
+mail.attach(audioAttachment)
+audio.close()
+
+
+message = mail.as_string()
+
+server.sendmail(os.getenv('EMAIL'),
+                os.getenv('TO'), message)
